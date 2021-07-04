c518c53db7fa4165ffe4599d17c826c664c7166d
Vinicius Patzer <viniciuspatzer2@gmail.com>
2021-07-04 02:24:52 -0300

    new commit

--- app.js
+++ app.js
@@ -6,30 +6,42 @@ const hexInput = document.querySelector('.hex');
 
 
 rgbInput.addEventListener('keyup', () => {
-    const term = filterValue(rgbInput.value);
+    const color = formatRGB(rgbInput.value);
+    const isColor = isValidColor(color);  // body.style.backgroundColor = hex;
 
+    if (isColor) {
+        const hex = RGBToHex(color);
+        hexInput.value = hex;
+    }
 });
 
 hexInput.addEventListener('keyup', () => {
-    const term = filterValue(hexInput.value);
+    const color = formatHEX(hexInput.value);
+    const isColor = isValidColor(color);  // body.style.backgroundColor = rgb;
 
+    if (isColor) {
+        const rgb = hexToRGB(color);
+        rgbInput.value = rgb;
+    }
 });
 
 
 
 
 
-function RGBToHex(r, g, b) {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
+function RGBToHex(rgb) {
+    let [r, g, b] = rgbInput.value.split(',').filter(c => parseInt(c));
+
+    r = Number(r).toString(16);
+    g = Number(g).toString(16);
+    b = Number(b).toString(16);
 
     if (r.length == 1) r = "0" + r;
     if (g.length == 1) g = "0" + g;
     if (b.length == 1) b = "0" + b;
 
     return "#" + r + g + b;
-}
+};
 
 
 function hexToRGB(hex) {
@@ -49,19 +61,20 @@ function hexToRGB(hex) {
     }
 
     return `rgb(${+r},${+g},${+b})`;
-}
+};
 
-
-function isValidColor(color) {
-    const el = document.createElement('div');
-    el.style.backgroundColor = color;
-    return el.style.backgroundColor ? true : false;
+function formatRGB(text) {
+    let [r, g, b] = text.split(',').filter(c => parseInt(c));
+    return `rgb(${r}, ${g}, ${b})`;
 };
 
 
-function filterValue(value) {
-    return value
-        .split('')
-        .filter(letter => letter.match(/[0-9]/i))
-        .join('');
+function formatHEX(text) {
+    return text.slice(0, 1) === '#' ? text : `#${text}`
+};
+
+
+function isValidColor(color) {
+    body.style.backgroundColor = color;
+    return body.style.backgroundColor ? true : false;
 };
